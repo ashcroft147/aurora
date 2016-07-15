991d8dbde5b69db2952dd98b5cbe5a15f52c4f2f
healess <healess@poscoict.com>
2016-07-15 11:41:19 +0900

    Make gitignore file.

--- src/express.js
+++ src/express.js
@@ -3,7 +3,6 @@ var MongoClient = require('mongodb').MongoClient
 var mongo  = require('mongodb')
 var app = express()
 
-
 //var mongoclient = new MongoClient(new Server("localhost", 27017));
 var url = 'mongodb://localhost:27017/test';
 
@@ -17,33 +16,33 @@ var server = app.listen(3000, function () {
 	console.log('Aurora Server running at http://%s:%s', host, port)
 })
 
+	// Connection URL
+	// Use connect method to connect to the Server
+		MongoClient.connect(url, function(err, db) {
+			console.log("Connected correctly to server");
+			var myCollection = db.collection('test');
+			//var cursor = myCollection.find({"title" : "First data", "user" : "king"});
+			var cursor = myCollection.find();
+			cursor.each(function(err, doc) {
 
-// Connection URL
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-	console.log("Connected correctly to server");
-	var myCollection = db.collection('test');
-	//var cursor = myCollection.find({"title" : "First data", "user" : "king"});
-	var cursor = myCollection.find();
-	cursor.each(function(err, doc) {
-		if(err){
-			console.log("document err:");
-			throw err;
-		}
-		if(doc==null){
-			console.log("document null:");
-            return;
-        }
-        console.log("document find:");
-        console.log(doc.title);
-    });
-	/*
-    myCollection.insert({"title" : "First data5", "user" : "king5" }, function(err, result) {
-    if(err)
-        throw err;
-    console.log("entry saved");
-	});*/
-	db.close();
-});
+				if(err){
+					console.log("document err:");
+					throw err;
+				}
 
+				if(doc == null){
+					console.log("document null:");
+		            return;
+		        }
+		        console.log("document find:");
+		        console.log(doc.title);
+		    });
+			/*
+		    myCollection.insert({"title" : "First data5", "user" : "king5" }, function(err, result) {
+		    if(err)
+		        throw err;
+		    console.log("entry saved");
+			});*/
+			db.close();
+		});
 // end
