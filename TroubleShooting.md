ae5495c11d60120225d62ad502ba28196304af33
ashcroft147 <pd050786@VDIPS.POSCO.DOM>
2016-07-28 18:54:28 +0900

    minor fix

--- TroubleShooting.md
+++ TroubleShooting.md
@@ -1,2 +1,4 @@
 1. express-react(MERN Stack) 에서는 Project의 Directory 구조를 어떻게 구성해야 할까 ?
- - express-generator
+ - express-generator 를 사용해서 초기 directory tree 구성
+ 
+2. 어떤 종류의 middleware component가 필요할까 ?
--- app.js
+++ app.js
@@ -17,7 +17,6 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
